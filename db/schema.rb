f03a61b67815ae8dadcf2ea78f77ebde482a0e9f
zjutimes <zjutimes@gmail.com>
2017-06-07 18:25:17 +0800

    add OrderMailer function

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,14 @@ ActiveRecord::Schema.define(version: 20170607095051) do
     t.integer  "cart_id"
     t.integer  "product_id"
     t.integer  "quantity",   default: 1
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
   end
 
   create_table "carts", force: :cascade do |t|
-
+    t.integer  "cart_id"
+    t.integer  "product_id"
+    t.integer  "quantity",   default: 1
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
   end
